4335bf5fd8f62331718542f2c2e81e89a0cffc3e
MaaarcosG <gut17909@uvg.edu.gt>
2021-03-03 13:16:23 -0600

    upadate binary

--- OneTimePad.py
+++ OneTimePad.py
@@ -5,11 +5,14 @@ import bitarray
 import sys
 
 
-def string_to_bitarray(data: str):
-    array = bitarray.bitarray()
-    array.frombytes(data.encode('utf-8'))
-    return array
-    
+def string_to_bitarray(data):
+    msg_bitarray = bitarray.bitarray()
+    msg_bitarray.frombytes(data.encode('utf-8')) 
+    return msg_bitarray
+
+def bit_to_string(data):
+    return ''.join(chr(int(data[i*8:i*8+8],2)) for i in range(len(data)/8))
+
 def xor(msg_array, key_array):
     data_array = []
     for x in range(len(msg_array)):
@@ -20,7 +23,7 @@ def xor(msg_array, key_array):
 def generate_key(data):
     key = []
     for _ in range(data):
-        key.append(random.choice(string.printable))
+        key.append(random.choice(['0', '1']))
     
     key = ''.join(key)
     #print(key)
@@ -30,6 +33,13 @@ def encrypt(msg):
     msg_array = string_to_bitarray(msg)
     key_array = string_to_bitarray(generate_key(len(msg)))
 
+    ''' 
+    Para binario 1 y 0'
+    msg_cifrado = xor(msg_array, key_array).to01()
+    key_gene = key_array.to01()
+    '''
+
+    ''' Para characters '''
     msg_cifrado = xor(msg_array, key_array).tobytes().decode('utf-8')
     key_gene = key_array.tobytes().decode('utf-8')
 
@@ -39,40 +49,39 @@ def encrypt(msg):
 def decrypt(cypher, key):
     cypher_array = bitarray.bitarray()
     cypher_array.frombytes(cypher.encode('utf-8'))
+    #print(cypher_array)
     key_array = bitarray.bitarray()
     key_array.frombytes(key.encode('utf-8'))
 
     # retornamos el mensaje descifrado
+    ''' 
+    Para binario 1 y 0
+    msg_descifrado = xor(cypher_array, key_array).to01()
+    '''
+
     msg_descifrado = xor(cypher_array, key_array).tobytes().decode('utf-8')
 
     return msg_descifrado
 
 def get_encrypt_file(filename):
-    try:
-        data_encrypt = open(filename, 'r').read()
-        
-        (plaintext, key) = encrypt(data_encrypt)
-        
-        with open('cypher.txt', 'w') as file:
-            file.write(plaintext)
-            
-        with open('key.txt', 'w') as file:
-            file.write(key)
-    except:
-        print('Error: %s' % sys.exc_info()[0])
-
+    data_encrypt = open(filename, 'r').read()
+    (plaintext, key) = encrypt(data_encrypt)
     
+    with open('cypher.txt', 'w+') as file:
+        file.write(plaintext)
+        
+    with open('key.txt', 'w+') as file:
+        file.write(key)
+
 def get_decrypt_file(filename, key_file):
-    try:
-        msg = open(filename, 'r').read()
-        key = open(key_file, 'r').read()
+    msg = open(filename, 'r').read()
+    key = open(key_file, 'r').read()
         
-        msg_descrypt = decrypt(msg, key)
+    msg_descrypt = decrypt(msg, key)
         
-        with open('decrypt.txt', 'w') as file:
-            file.write(msg_descrypt)
-    except:
-        print('Error: %s' % sys.exc_info()[0])
+    with open('decrypt.txt', 'w') as file:
+        file.write(msg_descrypt)
+
 
 if __name__ == '__main__':
     if(len(sys.argv) == 2):
