f5215df3af0adcb353847d6904cd24e4d593005a
MaaarcosG <gut17909@uvg.edu.gt>
2021-03-03 19:10:02 -0600

    add file txt

--- OneTimePad.py
+++ OneTimePad.py
@@ -10,9 +10,6 @@ def string_to_bitarray(data):
     msg_bitarray.frombytes(data.encode('utf-8')) 
     return msg_bitarray
 
-def bit_to_string(data):
-    return ''.join(chr(int(data[i*8:i*8+8],2)) for i in range(len(data)/8))
-
 def xor(msg_array, key_array):
     data_array = []
     for x in range(len(msg_array)):
@@ -24,10 +21,8 @@ def generate_key(data):
     key = []
     for _ in range(data):
         key.append(random.choice(['0', '1']))
-    
-    key = ''.join(key)
-    #print(key)
-    return key
+
+    return ''.join(key)
 
 def encrypt(msg):
     msg_array = string_to_bitarray(msg)
@@ -37,7 +32,6 @@ def encrypt(msg):
     msg_cifrado = xor(msg_array, key_array).to01()
     key_gene = key_array.to01()
 
-    # retorna en bytes el mensaje cifrado y la llave generada
     return msg_cifrado, key_gene
     
 def decrypt(cypher, key):
@@ -49,13 +43,14 @@ def decrypt(cypher, key):
     return msg_descifrado
 
 def get_encrypt_file(filename):
+    # abrimos el archivo
     data_encrypt = open(filename, 'r').read()
     (plaintext, key) = encrypt(data_encrypt)
     
-    with open('cypher.txt', 'w+') as file:
+    with open('cypher.txt', 'w') as file:
         file.write(plaintext)
         
-    with open('key.txt', 'w+') as file:
+    with open('key.txt', 'w') as file:
         file.write(key)
 
 def get_decrypt_file(filename, key_file):
